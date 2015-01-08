71c963bb8920c5929144ec00f04500f26f71be2a
ManjeetMurari <manjeet.murari@a-cti.com>
2015-01-08 18:46:27 +0530

    First time check in

---
+++ Simple/src/com/git/SimpleServlet.java
@@ -0,0 +1,13 @@
+package com.git;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class SimpleServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
