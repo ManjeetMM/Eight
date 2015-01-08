94707c2ffeb2d4828e1444a02a89ab2fe9ceab8b
ManjeetMurari <manjeet.murari@a-cti.com>
2015-01-08 19:13:05 +0530

    some changes

--- Simple/src/com/git/SimpleServlet.java
+++ Simple/src/com/git/SimpleServlet.java
@@ -1,6 +1,8 @@
 package com.git;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
@@ -9,5 +11,6 @@ public class SimpleServlet extends HttpServlet {
 			throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
+		PrintWriter out=resp.getWriter();
 	}
 }
